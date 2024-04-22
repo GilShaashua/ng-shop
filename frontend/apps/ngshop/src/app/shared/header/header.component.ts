64fd163768cea887cd3cd2be7687227315f545ed
Gil Shaashua <gilshaashua@gmail.com>
2024-04-23 02:20:09 +0300

    moved logic of cart cmp to header cmp

--- frontend/apps/ngshop/src/app/shared/header/header.component.ts
+++ frontend/apps/ngshop/src/app/shared/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsSearchComponent } from '@frontend/products';
+import { ProductsSearchComponent, ProductsService } from '@frontend/products';
 import { RouterModule } from '@angular/router';
-import { CartComponent, CartService } from '@frontend/orders';
+import { CartComponent, CartItem, CartService } from '@frontend/orders';
+import { Subject, firstValueFrom, switchMap, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'ngshop-header',
@@ -18,15 +19,63 @@ import { CartComponent, CartService } from '@frontend/orders';
     host: { class: 'full component-layout header-host' },
 })
 export class HeaderComponent implements OnInit {
-    constructor(private cartService: CartService) {}
+    constructor(
+        private cartService: CartService,
+        private productsService: ProductsService
+    ) {}
+
+    cart!: CartItem[] | null;
+    subscriptionSubject$ = new Subject();
     isCartShown = false;
     cartCount = 0;
 
     ngOnInit(): void {
         this.cartService.cart$.subscribe({
             next: (cart) => {
-                this.cartCount = this.cartService.cartCount || 0;
+                this.cartCount = cart?.length || 0;
             },
         });
+
+        this._getCart();
+    }
+
+    private _getCart() {
+        this.cartService.cart$
+            .pipe(
+                switchMap(async (cart) => {
+                    if (cart) {
+                        const modifiedCart = cart.map(async (cartItem) => {
+                            const product$ =
+                                this.productsService.getProductById(
+                                    cartItem.product as unknown as string
+                                );
+                            const product = await firstValueFrom(product$);
+                            const modifiedCartItem: CartItem = {
+                                product: product,
+                                quantity: cartItem.quantity,
+                            };
+                            return modifiedCartItem;
+                        });
+
+                        return await Promise.all(modifiedCart);
+                    }
+
+                    return null;
+                }),
+                takeUntil(this.subscriptionSubject$)
+            )
+            .subscribe({
+                next: (cart) => {
+                    this.cart = cart;
+                },
+                error: (err) => {
+                    console.error('Cannot get cart', err);
+                },
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptionSubject$.next(null);
+        this.subscriptionSubject$.complete();
     }
 }
--- frontend/libs/orders/src/lib/components/cart/cart.component.ts
+++ frontend/libs/orders/src/lib/components/cart/cart.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CartService } from '../../services/cart.service';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subject, firstValueFrom, switchMap, takeUntil } from 'rxjs';
-import { ProductsService } from '@frontend/products';
 import { CartItem } from '../../models/cart-item.model';
 
 @Component({
@@ -13,56 +10,6 @@ import { CartItem } from '../../models/cart-item.model';
     styleUrl: './cart.component.scss',
     host: { class: 'cart-host' },
 })
-export class CartComponent implements OnInit, OnDestroy {
-    constructor(
-        private cartService: CartService,
-        private productsService: ProductsService
-    ) {}
-
-    cart!: CartItem[] | null;
-    subscriptionSubject$ = new Subject();
-
-    ngOnInit(): void {
-        this._getCart();
-    }
-
-    private _getCart() {
-        this.cartService.cart$
-            .pipe(
-                switchMap(async (cart) => {
-                    if (cart) {
-                        const modifiedCart = cart.map(async (cartItem) => {
-                            const product$ =
-                                this.productsService.getProductById(
-                                    cartItem.product as unknown as string
-                                );
-                            const product = await firstValueFrom(product$);
-                            const modifiedCartItem: CartItem = {
-                                product: product,
-                                quantity: cartItem.quantity,
-                            };
-                            return modifiedCartItem;
-                        });
-
-                        return await Promise.all(modifiedCart);
-                    }
-
-                    return null;
-                }),
-                takeUntil(this.subscriptionSubject$)
-            )
-            .subscribe({
-                next: (cart) => {
-                    this.cart = cart;
-                },
-                error: (err) => {
-                    console.error('Cannot get cart', err);
-                },
-            });
-    }
-
-    ngOnDestroy(): void {
-        this.subscriptionSubject$.next(null);
-        this.subscriptionSubject$.complete();
-    }
+export class CartComponent {
+    @Input() cart!: CartItem[];
 }
