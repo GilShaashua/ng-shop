9ebbf7ce33c21a9819289ca425c01946d1a600ae
Gil Shaashua <gilshaashua@gmail.com>
2024-06-12 15:20:31 +0300

    started implementing search-products

--- frontend/libs/products/src/lib/components/products-search/products-search.component.ts
+++ frontend/libs/products/src/lib/components/products-search/products-search.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'ngshop-products-search',
-  standalone: true,
-  imports: [],
-  templateUrl: './products-search.component.html',
-  styleUrl: './products-search.component.scss'
+    selector: 'ngshop-products-search',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './products-search.component.html',
+    styleUrl: './products-search.component.scss',
 })
-export class ProductsSearchComponent {
+export class ProductsSearchComponent implements OnInit, OnDestroy {
+    constructor(private router: Router) {}
 
+    routePath = '';
+    routerUrlSubscription!: Subscription;
+    filterBy = {
+        name: '',
+    };
+
+    ngOnInit(): void {
+        this.routerUrlSubscription = this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                this.routePath = this.router.url;
+            }
+        });
+    }
+
+    onInputProducts() {
+        if (this.routePath !== '/products') {
+            this.router.navigate(['/products']);
+        }
+
+        console.log('filterBy', this.filterBy);
+    }
+
+    ngOnDestroy(): void {
+        this.routerUrlSubscription?.unsubscribe();
+    }
 }
