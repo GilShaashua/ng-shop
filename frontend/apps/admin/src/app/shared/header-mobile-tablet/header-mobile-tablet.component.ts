30f420d796bff00b3c93454f5f90a9f6efc9d2e4
Gil Shaashua <gilshaashua@gmail.com>
2024-06-25 00:23:28 +0300

    added header-tablet-mobile component && css

--- frontend/apps/admin/src/app/shared/header-mobile-tablet/header-mobile-tablet.component.ts
+++ frontend/apps/admin/src/app/shared/header-mobile-tablet/header-mobile-tablet.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@frontend/shared';
 
 @Component({
     selector: 'admin-header-mobile-tablet',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './header-mobile-tablet.component.html',
     styleUrl: './header-mobile-tablet.component.scss',
     host: { class: 'admin-header-mobile-tablet-host full component-layout' },
 })
 export class HeaderMobileTabletComponent {
+    constructor(private router: Router, private authService: AuthService) {}
+
     isNavOpen = false;
+
+    onLogout() {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+    }
 }
